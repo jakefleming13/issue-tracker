9eddca6fa3c6d5b109146f3e0c47066884255648
jakefleming13 <jakefleming@shaw.ca>
2024-09-28 11:23:56 -0600

    Build navBar component

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <a href="/">
+        <Image src={"/newLogo.png"} alt="Logo" width="64" height="64" />
+      </a>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
